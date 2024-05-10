9d3409c56d73d86ab48c16f15311a24f07ad3c1d
Pedrito el escamoso <PedroElEscamoso@mail.com>
2024-05-10 12:45:52 -0500

    escibí hola bbsita Brrrrrr

---
+++ JavaApplication64/src/javaapplication64/JavaApplication64.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication64;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplication64 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola bbsita Brrrrrrr");
+        // TODO code application logic here
+    }
+    
+}
